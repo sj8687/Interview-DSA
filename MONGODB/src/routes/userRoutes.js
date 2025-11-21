b58912e122040c62f82530fc53af44fbde012543
sj8687 <shreyashjadhav59807@gmail.com>
2025-11-21 19:54:04 +0530

    added

--- MONGODB/src/routes/userRoutes.js
+++ MONGODB/src/routes/userRoutes.js
@@ -26,72 +26,81 @@ router.post("/city", async (req, res) => {
 router.get("/", async (req, res) => {
   try {
 
+    const users = await User.find({    //and or nor in nin ...
+      // age: { $type: "number" }, //exists
+      $and: [
+        { age: { $gt: 18 } },
+        { age: { $gt: 50 } }
+      ]
+    });
+
+
     // const users  = await User.find().lean();  //return plan obj no metadata of mongo
 
-    const users = await User.aggregate([
+    // const users = await User.aggregate([
 
-      // { $match: { age: { $lt: 20 } } },  //gives lessthan 20 age collection match filter the docu.
-      // { $sort: { age: -1 } },            //gives sortes collection assending -1 , desending 1
-      // { $skip: 5 },                      //skip 5 docs
-      // { $limit: 1 },                     //only gives 1
-      // { $project: { name: 1, age: 1, _id: 0 } }         //only gives name and age ignore the id beause its 0
-
-
-      // {
-      //   $lookup: {           //join the two collections but throught the references like same id or name present in another one
-      //     from: "cities",     //collection name in monogo compass
-      //     localField: "name",    //from users
-      //     foreignField: "name",    //from city
-      //     as: "cityInfo"
-      //   },
-      // },
-      // {$unwind:"$cityInfo"}   //urn array → single object
+    // { $match: { age: { $lt: 20 } } },  //gives lessthan 20 age collection match filter the docu.
+    // { $sort: { age: -1 } },            //gives sortes collection assending -1 , desending 1
+    // { $skip: 5 },                      //skip 5 docs
+    // { $limit: 1 },                     //only gives 1
+    // { $project: { name: 1, age: 1, _id: 0 } }         //only gives name and age ignore the id beause its 0
 
 
-      // {
-      //   $bucket:{      //$bucket Auto Creates Groups Based on Boundaries
-      //     groupBy:"$age",            //group by age
-      //     boundaries:[0,10,20,30],       //Tum bas boundaries do, MongoDB automatically group bana dega.
-      //     default: "greater than 40",
-      //     output:{
-      //       count:{$sum:1}          //return the output
-      //     }
-      //   }
-      // }
+    // {
+    //   $lookup: {           //join the two collections but throught the references like same id or name present in another one
+    //     from: "cities",     //collection name in monogo compass
+    //     localField: "name",    //from users
+    //     foreignField: "name",    //from city
+    //     as: "cityInfo"
+    //   },
+    // },
+    // {$unwind:"$cityInfo"}   //urn array → single object
 
 
-      //  {
-      //     $match: {
-      //       $expr: { $gt: ["$marks", "$age"] }    //Returns true if value1 > value2 expr gives that power
-      //     }
-      //   }
+    // {
+    //   $bucket:{      //$bucket Auto Creates Groups Based on Boundaries
+    //     groupBy:"$age",            //group by age
+    //     boundaries:[0,10,20,30],       //Tum bas boundaries do, MongoDB automatically group bana dega.
+    //     default: "greater than 40",
+    //     output:{
+    //       count:{$sum:1}          //return the output
+    //     }
+    //   }
+    // }
+
+
+    //  {
+    //     $match: {
+    //       $expr: { $gt: ["$marks", "$age"] }    //Returns true if value1 > value2 expr gives that power
+    //     }
+    //   }
 
 
 
-      // {
-      //   $group: {    //only return what we typed 
-      //     _id:"$name",  //  // group by class
-      //     totalage:{$sum : "$age"}  //sum of age (gives us repeated names age)
-      //   }
-      // },
-
-      
-
-      // {
-      //   $match: { name: "shree" }
-      // },
-      // {
-      //   $group: {
-      //     _id: "$age",
-      //     sum: { $sum: 1 }
-      //   }
-      // }
+    // {
+    //   $group: {    //only return what we typed 
+    //     _id:"$name",  //  // group by class
+    //     totalage:{$sum : "$age"}  //sum of age (gives us repeated names age)
+    //   }
+    // },
 
 
-      
+
+    // {
+    //   $match: { name: "shree" }
+    // },
+    // {
+    //   $group: {
+    //     _id: "$age",
+    //     sum: { $sum: 1 }
+    //   }
+    // }
 
 
-    ]);
+
+
+
+    // ]);
 
     res.json({ users });
 
